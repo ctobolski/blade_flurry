375365f350d2b689827ee2cb31e9a45589fda5f1
Chris Tobolski <ctobolsk@ford.com>
2019-06-21 18:34:20 -0400

    Hey look, we can display text!

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,14 +1,24 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import fs from "fs";
+import { promisify } from "util";
 let styles = require("./Home.scss");
 
+const readFile = promisify(fs.readFile);
+
 export default function Home() {
+  const [text, setText] = useState();
+  useEffect(() => {
+    async function listDir() {
+      //passsing directoryPath and callback function
+      const contents = await readFile("/Users/chris/CombatLog.txt");
+      setText(contents.toString("UTF-8"));
+    }
+    listDir();
+  });
   return (
     <div>
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to="/counter">to Counter</Link>
+        {text}
       </div>
     </div>
   );
